1fb16052d2e731ed09fd24f1bac563f2579fd8e8
Lilian Ojo <loojo@umich.edu>
2019-09-17 10:22:22 -0400

    okay

---
+++ new_file.py
@@ -0,0 +1,6 @@
+last night
+today is a short dy for me
+when did she go to the store
+I had a fun weekend
+why are you sad? I don't know
+I need to creat a list of countries to visit in the next five years
